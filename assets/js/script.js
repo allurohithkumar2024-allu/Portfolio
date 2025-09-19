78ff9220c7f5d79fdb1128f35a746e81fb8971d7
allurohithkumar2024-allu <allurohithkumar2024@gmail.com>
2025-09-19 21:25:52 +0530

    changes in html css js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 // testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const testimonialsItem = document.querySelectorAll("[data-myguides-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -38,10 +38,10 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-myguides-avatar]").src;
+    modalImg.alt = this.querySelector("[data-myguide-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-myguides-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-myguides-text]").innerHTML;
 
     testimonialsModalFunc();
 
